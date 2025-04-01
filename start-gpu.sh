f2e806ac3c7bbbae6e4977dc3923ac81679f247a
fondoger <fondoger@outlook.com>
2025-04-01 21:09:55 +0800

    Modify start scripts

--- start-gpu.sh
+++ start-gpu.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# ./start-gpu.sh
+# ./start-gpu.sh 8881
+PORT=${1:-8880}
+
 # Get project root directory
 PROJECT_ROOT=$(pwd)
 
@@ -15,4 +19,13 @@ export WEB_PLAYER_PATH=$PROJECT_ROOT/web
 # Note: espeak may still require manual installation,
 uv pip install -e ".[gpu]"
 uv run --no-sync python docker/scripts/download_model.py --output api/src/models/v1_0
-uv run --no-sync uvicorn api.src.main:app --host 0.0.0.0 --port 8880
+# uv run --no-sync uvicorn api.src.main:app --host 0.0.0.0 --port 8880
+command="uv run --no-sync uvicorn api.src.main:app --host 0.0.0.0 --port $PORT"
+
+while true; do
+    echo "Starting Python script on port $PORT..."
+    $command
+    exit_code=$?
+    echo "Command exited with code $exit_code. Restarting..."
+    sleep 2  # Prevents excessive rapid restarts
+done
--- start-gpu_mac.sh
+++ start-gpu_mac.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
+# ./start-gpu.sh
+# ./start-gpu.sh 8881
+PORT=${1:-8880}
+
 # Get project root directory
 PROJECT_ROOT=$(pwd)
 
-# Create mps-specific venv directory
-VENV_DIR="$PROJECT_ROOT/.venv-mps"
-if [ ! -d "$VENV_DIR" ]; then
-    echo "Creating MPS-specific virtual environment..."
-    python3 -m venv "$VENV_DIR"
-fi
-
 # Set other environment variables
-export USE_GPU=true
-export USE_ONNX=false
-export PYTHONPATH=$PROJECT_ROOT:$PROJECT_ROOT/api
-export MODEL_DIR=src/models
-export VOICES_DIR=src/voices/v1_0
-export WEB_PLAYER_PATH=$PROJECT_ROOT/web
-
-# Set environment variables
 export USE_GPU=true
 export USE_ONNX=false
 export PYTHONPATH=$PROJECT_ROOT:$PROJECT_ROOT/api
@@ -32,4 +21,14 @@ export PYTORCH_ENABLE_MPS_FALLBACK=1
 
 # Run FastAPI with GPU extras using uv run
 uv pip install -e .
-uv run --no-sync uvicorn api.src.main:app --host 0.0.0.0 --port 8880
+uv run --no-sync python docker/scripts/download_model.py --output api/src/models/v1_0
+# uv run --no-sync uvicorn api.src.main:app --host 0.0.0.0 --port 8880
+command="uv run --no-sync uvicorn api.src.main:app --host 0.0.0.0 --port $PORT"
+
+while true; do
+    echo "Starting Python script on port $PORT..."
+    $command
+    exit_code=$?
+    echo "Command exited with code $exit_code. Restarting..."
+    sleep 2  # Prevents excessive rapid restarts
+done
